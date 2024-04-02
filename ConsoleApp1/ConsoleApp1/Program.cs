d98a9e835810369887fd8339ee8824b17da95f02
Josheldon Angeles <164700685+seniorgato7@users.noreply.github.com>
2024-04-02 13:41:14 +0800

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username, password;
+        int attempts = 0;
+
+        for (; attempts < 3;)
+        {
+            Console.Write("Enter your Username hehe : ");
+            username = Console.ReadLine();
+            Console.Write("Enter your Password hehe : ");
+            password = Console.ReadLine();
+
+            if (username == "Admin" && password == "Admin1234!")
+            {
+                Console.WriteLine("Login successful yey ^^");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username or password. Please try again lmao.");
+                attempts++;
+            }
+        }
+        if (attempts == 3)
+        {
+            Console.WriteLine("Maximum attempts reached. Try Harder.");
+        }
+    }
+}
